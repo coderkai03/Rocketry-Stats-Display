cc9a1a8c14bbf997ba3b1340076a0a68fbd82805
Riankai1073 <riancorci@gmail.com>
2023-10-31 17:19:44 -0700

    added the temperature displays

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ window = tk.Frame(root)
 window.pack()
 
 # Pressure/temp frames
-press_temp_frame = tk.Frame(window)
+press_temp_frame = tk.Frame(window, padx=50, pady=50)
 press_temp_frame.grid(row=0, column=0)
 
 # create pressure labels
@@ -17,7 +17,7 @@ pressure_displays=[]
 
 for i in range(3):
     label = tk.Label(
-        window,
+        press_temp_frame,
         text=f"Pressure {i + 1}: Placeholder",
         borderwidth=1,
         relief='solid',
@@ -25,6 +25,21 @@ for i in range(3):
         pady=5,
     )
     label.grid(row=i + 1, column=0)
+    pressure_displays.append(label)
+
+    # create pressure labels
+temperature_displays=[]
+
+for i in range(4):
+    label = tk.Label(
+        press_temp_frame,
+        text=f"Temperature {i + 1}: Placeholder",
+        borderwidth=1,
+        relief='solid',
+        padx=5,
+        pady=5,
+    )
+    label.grid(row=i, column=1)
     pressure_displays.append(label)
 
 # #create pressure Labels
